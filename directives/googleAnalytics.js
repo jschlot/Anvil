c252f6c9461ec340ebae8dc3df096a04149e4cc9
Jack Schlotthauer <jackschlotthauer@yahoo.com>
2014-09-07 03:15:54 -0700

    Update googleAnalytics.js

--- directives/googleAnalytics.js
+++ directives/googleAnalytics.js
@@ -1,6 +1,21 @@
 /*
 Google Analytics Tracking
 
+// Requirements
+//// google's tracking code must be loaded before this is executed
+//// something like <script src="/path/to/my/googleanalyticsobject.js"></script>
+
+//// This file usually looks like this:
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-##########-1', 'auto');
+
+//// End File
+
+
 PAGE VIEW TRACKING // "path" must start with a '/'
  gaTrack.page(); // uses default location and page title
  gaTrack.page({path: "/mandalorians", title: "Boba & Jango Fett" });
